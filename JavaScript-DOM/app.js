31c8370c4191eedf39fa1db8e719e5943e557fb3
AdrianMiftahulH <adrianmiftahul@gmail.com>
2023-02-06 10:51:37 +0700

    JS-DOM(replace element)

--- JavaScript-DOM/app.js
+++ JavaScript-DOM/app.js
@@ -98,8 +98,27 @@ li.setAttribute('title', 'new item')
 // ### add innerHTML ###
 li.innerHTML = '<a href="/" class="btn btn-danger btn-sm delete-item">x</a>';
 
-// Create append text node
+// ### Create append text node ### 
 li.appendChild(document.createTextNode("This New Text"))
 
-// execution new element
+// ### execution new element ###
 document.querySelector('ul.list-group').appendChild(li);
+
+// ## Replace Element ## 
+
+// Create element
+const newHeading = document.createElement('h3');
+
+// Add id
+newHeading.id = 'task-title';
+
+// New Text Node
+newHeading.appendChild(document.createTextNode('Task List'));
+
+// get old element
+const oldHeading = document.getElementById('card-title');
+// get parent
+const cardHeader = document.querySelector('.card-header');
+
+// Replace
+cardHeader.replaceChild(newHeading, oldHeading);
